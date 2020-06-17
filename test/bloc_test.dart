18e80e8bc864bfd815f69567a9592b2842d06f59
Zoran Stoilov <zoran@squarehost.co.uk>
2020-06-17 01:33:58 +0100

    Added more bloc tests

--- test/bloc_test.dart
+++ test/bloc_test.dart
@@ -1,6 +1,7 @@
 import 'package:bloc_test/bloc_test.dart';
 import 'package:date_checker_app/bloc/bloc.dart';
 import 'package:date_checker_app/database/models.dart';
+import 'package:date_checker_app/repository/auth_repository.dart';
 import 'package:date_checker_app/repository/batch_warning_repository.dart';
 import 'package:date_checker_app/repository/product_batch_repository.dart';
 import 'package:date_checker_app/repository/product_repository.dart';
@@ -8,21 +9,7 @@ import 'package:date_checker_app/repository/product_repository.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 
-class ProductBatchRepositoryMock extends Mock
-    implements ProductBatchRepository {}
-
-class ProductRepositoryMock extends Mock implements ProductRepository {}
-
-class ProductBatchBlocMock extends Mock implements ProductBatchBloc {}
-
-class ProductBlocMock extends Mock implements ProductBloc {}
-
-class BatchWarningRepositoryMock extends Mock
-    implements BatchWarningRepository {}
-
-class SyncBatchWarningBlocMock extends Mock implements SyncBatchWarningBloc {}
-
-class ProductSyncBlocMock extends Mock implements ProductSyncBloc {}
+import 'mocks.dart';
 
 void main() async {
   TestWidgetsFlutterBinding.ensureInitialized();
@@ -45,7 +32,6 @@ void main() async {
       build: () async => productBatchBloc,
       act: (bloc) => bloc.add(AllProductBatch()),
       expect: [
-        ProductBatchEmpty(),
         ProductBatchLoading(),
         AllProductBatchLoaded(),
       ],
@@ -56,7 +42,6 @@ void main() async {
       build: () async => productBatchBloc,
       act: (bloc) => bloc.add(SyncProductBatchData()),
       expect: [
-        ProductBatchEmpty(),
         ProductBatchLoading(),
         SyncProductBatchDataSuccess(),
       ],
@@ -67,7 +52,6 @@ void main() async {
       build: () async => productBatchBloc,
       act: (bloc) => bloc.add(OrderByExpiryDateEvent()),
       expect: [
-        ProductBatchEmpty(),
         ProductBatchLoading(),
         OrderedByExpiryDate(),
       ],
@@ -78,7 +62,6 @@ void main() async {
       build: () async => productBatchBloc,
       act: (bloc) => bloc.add(AddProductBatch()),
       expect: [
-        ProductBatchEmpty(),
         ProductBatchLoading(),
         ProductBatchAdded(),
       ],
@@ -106,7 +89,6 @@ void main() async {
       build: () async => productBloc,
       act: (bloc) => bloc.add(FetchAllProducts()),
       expect: [
-        ProductEmpty(),
         ProductLoading(),
         AllProductsLoaded(),
       ],
@@ -151,7 +133,6 @@ void main() async {
       build: () async => batchWarningBloc,
       act: (bloc) => bloc.add(AllBatchWarnings()),
       expect: [
-        BatchWarningEmpty(),
         BatchWarningLoading(),
         BatchWarningAllLoaded(),
       ],
@@ -162,7 +143,6 @@ void main() async {
       act: (bloc) =>
           bloc.add(EditQuantityEvent(quantity: 20, batchWarning: batchWarning)),
       expect: [
-        BatchWarningEmpty(),
         Success(),
       ],
     );
@@ -170,6 +150,201 @@ void main() async {
     tearDown(() {
       productBatchBloc?.close();
       batchWarningBloc?.close();
+    });
+  });
+
+  group('auth bloc test', () {
+    AuthenticationBloc authenticationBloc;
+    AuthRepository authRepository;
+    User user;
+    setUp(() {
+      authRepository = MockAuthRespository();
+      authenticationBloc = AuthenticationBloc(
+        authRepository: authRepository,
+      );
+      user = User(1, "zoran@123.com", "heythere", "Zoran", "Stoilov");
+    });
+
+    test('initial state is correct', () {
+      expect(authenticationBloc.initialState, AuthenticationInitial());
+    });
+
+    test('close does not emit new states', () {
+      expectLater(authenticationBloc,
+          emitsInOrder([AuthenticationInitial(), emitsDone]));
+      authenticationBloc.close();
+    });
+
+    group('AuthenticationStarted', () {
+      test('user not logged in state test', () {
+        final expectedResponse = [
+          AuthenticationInitial(),
+          AuthenticationFailure(),
+        ];
+        when(authRepository.isSignedIn())
+            .thenAnswer((_) => Future.value(false));
+        expectLater(authenticationBloc, emitsInOrder(expectedResponse));
+
+        authenticationBloc.add(AuthenticationStarted());
+      });
+      test('user logged in state test', () {
+        final expectedResponse = [
+          AuthenticationInitial(),
+          AuthenticationSuccess(user),
+        ];
+        when(authRepository.isSignedIn()).thenAnswer((_) => Future.value(true));
+        when(authRepository.getLoggedUser())
+            .thenAnswer((_) => Future.value(user));
+        expectLater(authenticationBloc, emitsInOrder(expectedResponse));
+
+        authenticationBloc.add(AuthenticationStarted());
+      });
+
+      test('user logged in state test', () {
+        final expectedResponse = [
+          AuthenticationInitial(),
+          AuthenticationSuccess(user),
+        ];
+        when(authRepository.isSignedIn()).thenAnswer((_) => Future.value(true));
+        when(authRepository.getLoggedUser())
+            .thenAnswer((_) => Future.value(user));
+        expectLater(authenticationBloc, emitsInOrder(expectedResponse));
+
+        authenticationBloc.add(AuthenticationStarted());
+      });
+    });
+
+    group('AuthenticationLoggedIn', () {
+      test('get logged in user', () {
+        final expectedResponse = [
+          AuthenticationInitial(),
+          AuthenticationSuccess(user),
+        ];
+        when(authRepository.getLoggedUser())
+            .thenAnswer((_) => Future.value(user));
+        expectLater(authenticationBloc, emitsInOrder(expectedResponse));
+        authenticationBloc.add(AuthenticationLoggedIn());
+      });
+    });
+
+    group('AuthenticationLoggedOut', () {
+      test('test AuthenticationLoggedOut event', () {
+        final expectedResponse = [
+          AuthenticationInitial(),
+          AuthenticationFailure(),
+        ];
+        when(authRepository.signOut()).thenAnswer((_) => Future.value(null));
+        expectLater(authenticationBloc, emitsInOrder(expectedResponse));
+        authenticationBloc.add(AuthenticationLoggedOut());
+      });
+    });
+
+    tearDown(() {
+      authenticationBloc?.close();
+    });
+  });
+
+  group('LoginBloc', () {
+    LoginBloc loginBloc;
+    AuthRepository authRepository;
+
+    setUp(() {
+      authRepository = MockAuthRespository();
+      loginBloc = LoginBloc(authRepository: authRepository);
+    });
+
+    test('check correct initial state', () {
+      expect(loginBloc.initialState.isSuccess, LoginState.initial().isSuccess);
+    });
+
+    blocTest(
+      'login email changed event',
+      build: () async => loginBloc,
+      act: (bloc) => bloc.add(LoginEmailChanged()),
+      expect: [],
+    );
+
+    blocTest(
+      'login password changed event',
+      build: () async => loginBloc,
+      act: (bloc) => bloc.add(LoginPasswordChanged()),
+      expect: [],
+    );
+
+    blocTest(
+      'login password changed event',
+      build: () async => loginBloc,
+      act: (bloc) => bloc.add(LoginWithCredentialsPressed()),
+      expect: [
+        isA<LoginState>()
+            .having((state) => state.isSubmitting, 'is submitting true', true),
+        isA<LoginState>()
+            .having((state) => state.isSuccess, 'login passed', true),
+      ],
+    );
+    tearDown(() {
+      loginBloc?.close();
+    });
+  });
+
+  group('LoggedOutBloc', () {
+    LoggedOutBloc loggedOutBloc;
+    AuthenticationBloc authBloc;
+    setUp(() {
+      authBloc = MockAuthBloc();
+      loggedOutBloc = LoggedOutBloc(
+        authBloc: authBloc,
+      );
+    });
+
+    test('check initial state is correct', () {
+      expect(
+        loggedOutBloc.initialState,
+        isA<IsNotLoggedOut>(),
+      );
+    });
+
+    blocTest(
+      'on logged out event',
+      build: () async => loggedOutBloc,
+      act: (bloc) => bloc.add(LogOutPressed()),
+      expect: [
+        isA<IsLoggedOut>(),
+      ],
+    );
+
+    tearDown(() {
+      loggedOutBloc?.close();
+      authBloc?.close();
+    });
+  });
+
+  group('NotificationsBloc', () {
+    NotificationsBloc notificationsBloc;
+    ProductSyncBloc productSyncBloc;
+    SyncBatchWarningBloc syncBatchWarningBloc;
+    setUp(() {
+      productSyncBloc = ProductSyncBlocMock();
+      syncBatchWarningBloc = SyncBatchWarningBlocMock();
+      notificationsBloc = NotificationsBloc(
+        productSyncBloc: productSyncBloc,
+        syncBatchWarningBloc: syncBatchWarningBloc,
+      );
+    });
+
+    blocTest(
+      'test AddNewNotification',
+      build: () async => notificationsBloc,
+      act: (bloc) => bloc.add(AddNewNotification()),
+      expect: [
+        isA<DisplayNotification>(),
+      ],
+    );
+
+    tearDown(() {
+      notificationsBloc?.close();
+      productSyncBloc?.close();
+      syncBatchWarningBloc?.close();
     });
   });
 }
